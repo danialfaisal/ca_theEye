27f5f4763f01c9371f9fe66f415cfde97d7ad61a
danialfaisal <dfaisal@unomaha.edu>
2021-02-23 00:15:13 -0600

    added admin class

--- theEye/tracking/admin.py
+++ theEye/tracking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Events
 
-# Register your models here.
+class EventList(admin.ModelAdmin):
+    list_display = ('session_id', 'category', 'name', 'data', 'timestamp')
+    list_filter = ('session_id', 'category', 'timestamp')
+    search_fields = ('session_id', 'category', 'timestamp')
+    ordering = ['timestamp']
+
+admin.site.register(Events, EventList)
